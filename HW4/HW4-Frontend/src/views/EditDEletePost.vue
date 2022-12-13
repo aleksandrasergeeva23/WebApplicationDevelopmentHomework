12c88f58313376a22c13bef09c82c00ee7bcfa54
dianajunus <dishr@hotmail.com>
2022-12-13 12:01:26 +0200

    AddPost and EditDelitePage creation

---
+++ HW4/HW4-Frontend/src/views/EditDEletePost.vue
@@ -0,0 +1,135 @@
+<template>
+    <div class="signup-box">
+    <h3>Edit or delete post</h3>
+    <form>
+      <div class="user-box">
+        <input type="text" name="body1">
+        <label for="email">Edit body: </label>
+      </div>
+      
+      <div class="container">
+        <button @click="EditPost"  class="center">Edite post</button>
+        <button @click="DeletePost" class="center">Delete post</button>
+      </div>
+    </form>
+  </div>
+  </template>
+  
+  <script>
+import { setTimeout } from 'timers';
+
+  export default {
+  name: "body", 
+  
+  data: function() {
+      return {
+     body1: '',
+     time,
+     
+    }
+    },
+    methods: {
+  
+  
+  EditPost() {
+        var data = {
+          body1: this.body1,
+          time:setTimeout,
+        };
+        // using Fetch - post method - send an HTTP post request to the specified URI with the defined body
+        fetch("http://localhost:3000/#/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+            credentials: 'include', //  Don't forget to specify this if you need cookies
+            body: JSON.stringify(data),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data);
+        this.$router.push("/");
+        //location.assign("/");
+        })
+        .catch((e) => {
+          console.log(e);
+          console.log("error");
+        });
+      },
+    }, 
+    }
+  </script>
+
+<style scoped>
+.signup-box {
+  max-width: 420px;
+  margin: 30px auto;
+  background: rgba(218, 219, 224, 0.534);
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+h3 {
+  text-align: center;
+  color: rgb(255, 255, 255);
+}
+
+.signup-box .user-box {
+  position: relative;
+}
+.signup-box .user-box input {
+  width: 100%;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  margin-bottom: 30px;
+  border: none;
+  border-bottom: 1px solid #fff;
+  outline: none;
+  background: transparent;
+}
+.signup-box .user-box label {
+  position: absolute;
+  top:0;
+  left: 0;
+  padding: 10px 0;
+  font-size: 16px;
+  color: #fff;
+  pointer-events: none;
+  transition: .5s;
+}
+
+.signup-box .user-box input:focus ~ label,
+.signup-box .user-box input:valid ~ label {
+  top: -20px;
+  left: 0;
+  color: #03e9f4;
+  font-size: 12px;
+}
+
+
+
+button {
+  background: rgb(32, 60, 160);
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+  align-items: center;
+  text-align: center;
+}
+
+
+.center {
+  margin: auto;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  width: 30%; 
+}
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
